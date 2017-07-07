1a38fc2606713bbc3c5072114849b0cec9774659
Akshitha <akshi0608@gmail.com>
2017-07-07 16:55:00 +0530

    job Details

--- ATOS_IAF_App/app/src/main/java/com/example/sb/atos_iaf_app/Homepage.java
+++ ATOS_IAF_App/app/src/main/java/com/example/sb/atos_iaf_app/Homepage.java
@@ -1,46 +1,50 @@
 package com.example.sb.atos_iaf_app;
 
-import android.graphics.Color;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
-import android.os.Message;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TabHost;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
-import android.content.Intent;
-import java.util.HashMap;
-import android.widget.ListView;
-import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import static android.graphics.Color.WHITE;
+import com.example.sb.atos_iaf_app.Sql.DataBaseHelper1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class Homepage extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    private Context context;
+    DataBaseHelper1 dbhelper;
     Session session;
-    String UpcomingInterviewsArray[] = {"Interview1: Jobi_d1","Interview2: Jobi_d2","Interview3: Jobi_d1"};
-    String AcceptedInterviewsArray[] = {"Interview1: jobid3","Interwiew1: jobid4"};
-    String TodaysInterviewArray[] = {"Interview2:jobid4"};
-    String CompletedInterview[] = {"interview3:job3","interview:job5",};
+   // String UpcomingInterviewsArray[] = {"Interview1: Jobi_d1","Interview2: Jobi_d2","Interview3: Jobi_d1"};
+   // String AcceptedInterviewsArray[] = {"Interview1: jobid3","Interwiew1: jobid4"};
+   // String TodaysInterviewArray[] = {"Interview2:jobid4"};
+   // String CompletedInterview[] = {"interview3:job3","interview:job5",};
 
-    protected void onCreate(Bundle savedInstanceState)
-    {
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
         session = new Session(getApplicationContext());
-
+        context = this;
+        dbhelper = new DataBaseHelper1(context);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -56,7 +60,7 @@ public class Homepage extends AppCompatActivity
         TextView lblPassword = (TextView) findViewById(R.id.password);
 
         // Button logout
-       // btnLogout = (Button) findViewById(R.id.logout);
+        // btnLogout = (Button) findViewById(R.id.logout);
 
         Toast.makeText(getApplicationContext(), "User Login Status: " + session.isLoggedIn(), Toast.LENGTH_LONG).show();
         /**
@@ -73,12 +77,12 @@ public class Homepage extends AppCompatActivity
         String name = user.get(Session.KEY_NAME);
 
         // email
-        Log.d("user",name);
+        Log.d("user", name);
         String password = user.get(Session.KEY_PASSWORD);
         System.out.print(name);
 
         // displaying user data
-      // lblName.setText(Html.fromHtml("Name: <b>" + name + "</b>"));
+        // lblName.setText(Html.fromHtml("Name: <b>" + name + "</b>"));
         //lblPassword.setText(Html.fromHtml("Passwo: <b>" + password + "</b>"));
 
 
@@ -97,7 +101,7 @@ public class Homepage extends AppCompatActivity
         });*/
 
         // create the TabHost that will contain the Tabs
-        TabHost tabHost = (TabHost)findViewById(R.id.tabhost);
+        TabHost tabHost = (TabHost) findViewById(R.id.tabhost);
         tabHost.setup();
 
         TabSpec tab1 = tabHost.newTabSpec("Upcoming Interviews");
@@ -134,6 +138,75 @@ public class Homepage extends AppCompatActivity
             TextView tv = (TextView) tabHost.getTabWidget().getChildAt(i).findViewById(R.id.title);
             tv.setTextColor(0xffffffff);
         }*/
+        SQLiteDatabase db = dbhelper.getReadableDatabase();
+        // Start the transaction.
+        db.beginTransaction();
+        List<String> UpcomingInterviewsArray = new ArrayList<String>();
+        List<String> AcceptedInterviewsArray = new ArrayList<String>();
+
+        List<String> TodaysInterviewArray= new ArrayList<String>();
+        List<String> CompletedInterview= new ArrayList<String>();
+        try {
+
+            // String selectQuery = " SELECT JobID,JobDescription,AppID FROM  jobdetails where Status = ' NO ' " ;
+            String status1 = "NO";
+            String status2 = "Accepted";
+            String status3="Completed";
+            String[] columns = {"JobID", "JobDescription", "AppID"};
+           Cursor cursor = db.query("jobdetails", columns, "status=?", new String[]{status1}, null, null, null);
+
+            Log.d("DD", "ss1");
+            if (cursor.getCount() > 0) {
+                Log.d("DD", "ss");
+                while (cursor.moveToNext()) {
+                    // Read columns data
+                    String JID = cursor.getString(cursor.getColumnIndex("JobID"));
+                    String JobDes = cursor.getString(cursor.getColumnIndex("JobDescription"));
+                    String ApID = cursor.getString(cursor.getColumnIndex("AppID"));
+                    UpcomingInterviewsArray.add(JID+"  "+JobDes+"  "+ApID);
+                }
+
+            }
+            Cursor cursor1=db.query("jobdetails", columns, "status=?", new String[]{status2}, null, null, null);
+            Log.d("DD", "ss1");
+            if (cursor1.getCount() > 0) {
+                Log.d("DD", "ss");
+                while (cursor1.moveToNext()) {
+                    // Read columns data
+                    String JID = cursor1.getString(cursor1.getColumnIndex("JobID"));
+                    String JobDes = cursor1.getString(cursor1.getColumnIndex("JobDescription"));
+                    String ApID = cursor1.getString(cursor1.getColumnIndex("AppID"));
+                    AcceptedInterviewsArray.add(JID+"  "+JobDes+"  "+ApID);
+                }
+
+            }
+            Cursor cursor2=db.query("jobdetails", columns, "status=?", new String[]{status3}, null, null, null);
+            Log.d("DD", "ss1");
+            if (cursor1.getCount() > 0) {
+                Log.d("DD", "ss");
+                while (cursor2.moveToNext()) {
+                    // Read columns data
+                    String JID = cursor2.getString(cursor1.getColumnIndex("JobID"));
+                    String JobDes = cursor2.getString(cursor1.getColumnIndex("JobDescription"));
+                    String ApID = cursor2.getString(cursor1.getColumnIndex("AppID"));
+                    CompletedInterview.add(JID+"  "+JobDes+"  "+ApID);
+                }
+
+            }
+            db.setTransactionSuccessful();
+        }
+        catch (SQLiteException e)
+        {
+            e.printStackTrace();
+
+        }
+        finally
+        {
+            db.endTransaction();
+                    // End the transaction.
+            db.close();
+                    // Close database
+        }
 
             ListView listView1 = (ListView) findViewById(R.id.UpInterview);
             ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this,
@@ -141,22 +214,23 @@ public class Homepage extends AppCompatActivity
             listView1.setAdapter(adapter1);
 
 
-             ListView listView2 = (ListView) findViewById(R.id.AccInterview);
+            ListView listView2 = (ListView) findViewById(R.id.AccInterview);
             ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
                     R.layout.content_third_listacc,R.id.listacc, AcceptedInterviewsArray);
             listView2.setAdapter(adapter2);
 
-            ListView listView3 = (ListView) findViewById(R.id.TodInterview);
+         /*   ListView listView3 = (ListView) findViewById(R.id.TodInterview);
             ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this,
                     R.layout.content_third_listtod,R.id.listtod, TodaysInterviewArray);
             listView3.setAdapter(adapter3);
-
+*/
             ListView listView4 = (ListView) findViewById(R.id.ComInterview);
             ArrayAdapter<String> adapter4 = new ArrayAdapter<String>(this,
                     R.layout.content_third_listcom,R.id.listcom, CompletedInterview);
             listView4.setAdapter(adapter4);
 
-        }
+    }
+
 
 
     @Override
